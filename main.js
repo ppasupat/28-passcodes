c5ca045e942445183fc472818d1c70cfcaab3c4f
Panupong (Ice) Pasupat <panuponggong@gmail.com>
2021-02-03 13:50:33 +0700

    Copied stuff from last year

---
+++ main.js
@@ -0,0 +1,104 @@
+$(function () {
+  "use strict";
+
+  const SCREEN_WIDTH = 500, SCREEN_HEIGHT = 700;
+  const FRAME_RATE = 25;
+
+  // ################################
+  // Utilities
+
+  function showScene(name) {
+    $('.scene').hide();
+    $('#scene-' + name).show();
+  }
+
+  function showCover(name) {
+    $('#cover-wrapper').show();
+    $('.cover').hide();
+    if (name) {
+      $('#cover-' + name).show();
+    }
+  }
+
+  function hideCover() {
+    $('#cover-wrapper').hide();
+  }
+
+  // ################################
+  // Persistence
+
+  const APP_NAME = 'secret-code-28';
+  let settings = {};
+
+  function loadSettings() {
+    let settings_raw = localStorage.getItem(APP_NAME);
+    if (settings_raw === null) {
+      // TODO: Change this
+      settings = {hard_mode: 0};
+    } else {
+      settings = JSON.parse(settings_raw);
+    }
+  }
+
+  function saveSettings() {
+    try {
+      localStorage.setItem(APP_NAME, JSON.stringify(settings));
+    } catch (e) {
+      alert("ERROR: " + e.message);
+    }
+  }
+
+  loadSettings();
+
+  // ################################
+  // Game logic
+
+
+  // ################################
+  // Main menu
+
+
+  // ################################
+  // Preloading and screen resizing
+
+  function resizeScreen() {
+    let ratio = Math.min(
+      1.0,
+      window.innerWidth / SCREEN_WIDTH,
+      (window.innerHeight - 25) / SCREEN_HEIGHT,
+    );
+    $('#game-wrapper').css({
+      'width': (SCREEN_WIDTH * ratio) + 'px',
+      'height': (SCREEN_HEIGHT * ratio) + 'px',
+    });
+    $('#game').css('transform', 'scale(' + ratio + ')');
+  }
+
+  resizeScreen();
+  $(window).resize(resizeScreen);
+
+  const imageList = [
+  ];
+  let numResourcesLeft = imageList.length;
+  $('#pane-loading').text('Loading resources (' + numResourcesLeft + ' left)');
+
+  function decrementPreload () {
+    numResourcesLeft--;
+    if (numResourcesLeft === 0) {
+      setupMenu();
+      showScene('menu');
+    } else {
+      $('#pane-loading').text('Loading resources (' + numResourcesLeft + ' left)');
+    }
+  }
+
+  let images = [];
+  imageList.forEach(function (x) {
+    let img = new Image();
+    img.onload = decrementPreload;
+    img.src = x;
+    images.push(img);
+  });
+  showScene('preload');
+
+});
